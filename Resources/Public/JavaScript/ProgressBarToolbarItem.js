4491d4038b3c3cbef9237dcce52cc07ad0e23508
Tizian Schmidlin <st@cabag.ch>
2020-11-02 17:12:05 +0100

    [TASK] Fix indentation

--- Resources/Public/JavaScript/ProgressBarToolbarItem.js
+++ Resources/Public/JavaScript/ProgressBarToolbarItem.js
@@ -33,22 +33,22 @@ require(['jquery', 'TYPO3/CMS/Core/Ajax/AjaxRequest'], function (jquery, AjaxReq
         }
 
         var $container = jquery('#t3-backend-progress-container');
-        if(renderFunction) {
-          renderFunction($container, jsonData);
+        if (renderFunction) {
+            renderFunction($container, jsonData);
         } else {
-          $container.empty();
+            $container.empty();
           // TODO: move this to some js templating engine?
-          for (item in jsonData) {
-            // Do some rendering
-            $container.append(
-              `<div class="t3-backend-progress-item">
-                <div class="t3-backend-progress-bar-container">
-                    <div style="width:${item['currentStep'] / item['steps']}%;" class="t3-backend-progress-bar"></div>
-                    <div class="t3-backend-progress-bar-label">${item['label']}</div>
-                </div>
-              </div>`
-            );
-          }
+            for (item in jsonData) {
+              // Do some rendering
+                $container.append(
+                    ` < div class = "t3-backend-progress-item" >
+                    < div class = "t3-backend-progress-bar-container" >
+                    < div style = "width:${item['currentStep'] / item['steps']}%;" class = "t3-backend-progress-bar" > < / div >
+                    < div class = "t3-backend-progress-bar-label" > ${item['label']} < / div >
+                    <  / div >
+                    <  / div > `
+                );
+            }
         }
     }
 
