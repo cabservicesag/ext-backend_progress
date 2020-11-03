55da6bf4a4c0313075b97d2e8e15ae16c37b0387
Tizian Schmidlin <st@cabag.ch>
2020-11-03 11:57:53 +0100

    [TASK] Set correct dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.6-10.4.99',
+            'typo3' => '9.5.6-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => []
