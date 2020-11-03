aed62b0488d9437652d568ad8ddfb59c36eabb22
Tizian Schmidlin <st@cabag.ch>
2020-11-03 11:54:24 +0100

    [FEATURE] Add icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,13 @@ defined('TYPO3_MODE') or die();
 (function () {
     // Add the menu toolbar
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1604323154] = \Cabag\BackendProgress\Toolbar\TaskProgressToolbarItem::class;
+
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    );
+    $iconRegistry->registerIcon(
+        'tx-backend-progress-icon', // Icon-Identifier, e.g. tx-myext-action-preview
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:backend_progress/Resources/Public/Icons/Extension.svg']
+    );
 })();
