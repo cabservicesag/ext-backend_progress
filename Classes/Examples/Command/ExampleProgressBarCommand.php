e1bbfffa8fdeb27f431e47b78dbca1f8881b0b61
Tizian Schmidlin <st@cabag.ch>
2020-11-03 09:21:06 +0100

    [TASK] Finish example

--- Classes/Examples/Command/ExampleProgressBarCommand.php
+++ Classes/Examples/Command/ExampleProgressBarCommand.php
@@ -29,11 +29,11 @@ class ExampleProgressBarCommand extends Command
     {
         // Start the task
         $this->startTask();
-        sleep(10);
+        sleep(1);
 
         // Go to the next step
         $this->nextStep();
-        sleep(10);
+        sleep(1);
 
         // Update the step label
         $this->updateStepLabel('Executing part one of this step');
@@ -41,35 +41,35 @@ class ExampleProgressBarCommand extends Command
         $array = [];
         // Do some more for find $array
         // ...
-        $array = range(0, 100);
+        $array = range(1, 100);
 
         // Update in a loop
         $amount = count($array);
         mt_srand(time());
         foreach ($array as $key => $value) {
-            sleep(mt_rand(0,2));
+            usleep(mt_rand(10000,200000));
             $this->updateStepLabel(sprintf('Update part %s from %d', $key, $amount));
             // Do some processing
         }
-        sleep(10);
+        sleep(1);
         // Go to the next step and update label
         $this->nextStep('Label for whole task (can be updated)');
-        sleep(3);
+        sleep(2);
 
         $this->nextStep('This is the last step');
-        sleep(5);
+        sleep(1);
 
         // If you execute nextStep too often, the total amount of steps will raise and the completion will stay at
         // steps-1 until endTask is called
         $this->nextStep('An additional step(what happened here?)');
-        sleep(11);
+        sleep(2);
 
         // End the task
         $this->endTask();
 
         // Set up how long a task should stay in the queue after finishing (default 60 seconds)
-        $this->cleanupTask(30);
+        $this->cleanupTask(10);
         // Negative values are put into absolute values, so this is equivalent
-        $this->cleanupTask(-30);
+        $this->cleanupTask(-10);
     }
 }
