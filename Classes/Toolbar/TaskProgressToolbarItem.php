8d0927a25ff51b2f4f453688c52e8476b8c89aef
Tizian Schmidlin <st@cabag.ch>
2020-11-03 11:54:24 +0100

    [FEATURE] Add icon

--- Classes/Toolbar/TaskProgressToolbarItem.php
+++ Classes/Toolbar/TaskProgressToolbarItem.php
@@ -3,6 +3,7 @@
 namespace Cabag\BackendProgress\Toolbar;
 
 use TYPO3\CMS\Backend\Toolbar\ToolbarItemInterface;
+use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -22,8 +23,18 @@ class TaskProgressToolbarItem implements ToolbarItemInterface
 
     public function getItem()
     {
+        /** @var IconFactory $iconFactory */
+        $iconFactory = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            \TYPO3\CMS\Core\Imaging\IconFactory::class
+        );
+        $icon = $iconFactory->getIcon(
+            'tx-backend-progress-icon',
+            \TYPO3\CMS\Core\Imaging\Icon::SIZE_SMALL
+        );
         // TODO: Make this as fluid template (icon)
-        return '<span id="t3-backend-backend-progress" class="toolbar-item-icon" title="Backend Task Progress"></span><span class="toolbar-item-title"></span>';
+        return '
+                <span id="t3-backend-backend-progress" class="toolbar-item-icon" title="Backend Task Progress">' . $icon . '</span>
+                <span class="toolbar-item-title"></span>';
     }
 
     public function hasDropDown()
